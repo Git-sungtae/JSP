288c6bcdb6b9e34420c80409ccbebd33570129b7
JangSungtae <stj0905@gmail.com>
2023-03-13 21:17:54 +0900

    0313

--- oJSP/src/main/webapp/CH19/ch01/memo.js
+++ oJSP/src/main/webapp/CH19/ch01/memo.js
@@ -1,1 +1,8 @@
-
+	const textVal =  $('p').text(); //Hi there!
+	const htmlVal =  $('p').html(); //<p> Hi there! </p>
+	const valVal =  $('p').val(); // form 입력값
+	
+	$('div').append(textVal);
+	$('div').append(htmlVal);
+	$('div').append(valVal);
+	
